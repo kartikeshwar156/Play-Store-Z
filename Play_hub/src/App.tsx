444b435552c7348fdec9a3ad7f4ef84a36c4059e
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-02-26 12:10:28 +0530

    Build a Responsive Layout

--- Play_hub/src/App.tsx
+++ Play_hub/src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
-const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
-};
+function App() {
+  // in below line you can see back tick because we are inserting double quotation marks inside them
+  return (
+    <Grid
+      templateAreas={{
+        base: ` "nav" "aside" `,
+        lg: ` "nav nav" "aside main" `,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        aside
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="main" bg="blue">
+          main
+        </GridItem>
+      </Show>
+    </Grid>
+  ); // First row will have two columns 'nav' and 'nav', 2nd row will have two columns 'aside' and 'main'
+}
 
 export default App;
