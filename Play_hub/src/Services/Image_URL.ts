45a3ab8430195cf6a2b053de37ec0b3189e4a299
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-06 16:23:31 +0530

    Handle Games With no Image

--- Play_hub/src/Services/Image_URL.ts
+++ Play_hub/src/Services/Image_URL.ts
@@ -1,7 +1,8 @@
+import noImage from '../assets/NoImage.webp'
 
 const getCroppedImage_URL = (url: string) => {
 
-  if(!url) return '';
+  if(!url) return noImage;
 
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
