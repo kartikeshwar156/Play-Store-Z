8edfec5163b8d34e197969e9aeafc3f6b4bd1b5d
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-08 20:04:31 +0530

    Adding Emojis

--- Play_hub/src/Components/GameCard.tsx
+++ Play_hub/src/Components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, Image, CardBody, Heading, Text, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImage_URL from "../Services/Image_URL";
+import Emoji from "./Emoji";
 
 interface props {
   game: Game;
@@ -22,7 +23,7 @@ const GameCard = ({ game }: props) => {
             {/* // constructing an  array of platform object */}
             <CriticScore score={game.metacritic}></CriticScore>
           </HStack>
-          <Heading fontSize="xl">{game.name}</Heading>
+          <Heading fontSize="xl">{game.name} <Emoji rating={game.rating_top}></Emoji></Heading>
         </CardBody>
       </Card>
     </>
