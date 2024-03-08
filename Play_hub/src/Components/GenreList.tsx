39cd98387658b686482da1347c18f4373abdb589
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-08 18:52:07 +0530

    Cleaning the Genres

--- Play_hub/src/Components/GenreList.tsx
+++ Play_hub/src/Components/GenreList.tsx
@@ -1,29 +1,53 @@
-import { HStack, List, ListItem, Image, Button, Spinner} from '@chakra-ui/react';
-import useGenres, { Genre } from '../Hooks/useGenres'
-import getCroppedImage_URL from '../Services/Image_URL';
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Button,
+  Spinner,
+  Heading,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../Hooks/useGenres";
+import getCroppedImage_URL from "../Services/Image_URL";
 
-interface props{
-	onSelectedGenre: (genre: Genre) => void;
+interface props {
+  onSelectedGenre: (genre: Genre) => void;
 }
 
-const GenreList = ({onSelectedGenre}: props) => {
-  const {data, error, Loading, setLoading} = useGenres();
+const GenreList = ({ onSelectedGenre }: props) => {
+  const { data, error, Loading, setLoading } = useGenres();
 
-  if(error) return null
-  if(Loading) return <Spinner></Spinner>;
+  if (error) return null;
+  if (Loading) return <Spinner></Spinner>;
 
   return (
-	 <>
-	 <List>
-		{data.map(genre => <ListItem key={genre.id} paddingY='5px'>
-			<HStack>
-				<Image boxSize='32px' borderRadius='8px' src={ getCroppedImage_URL(genre.image_background)}></Image>
-				<Button onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
-			</HStack>
-			</ListItem>)}
-	 </List>
-	 </>
-  )
-}
+    <>
+      <Heading fontSize='2xl' marginBottom='3px'>Genres</Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                objectFit="cover"
+                borderRadius="8px"
+                src={getCroppedImage_URL(genre.image_background)}
+              ></Image>
+              <Button
+                whiteSpace={"normal"}
+                textAlign="left"
+                onClick={() => onSelectedGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+};
 
-export default GenreList
+export default GenreList;
