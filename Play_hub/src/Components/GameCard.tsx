c607163c396696b071472c9904a297c72f9f3a7a
kartikeshwar <kartikeshwarhingole@gmail.com>
2024-03-08 18:55:22 +0530

    Cleaning up the Game Card

--- Play_hub/src/Components/GameCard.tsx
+++ Play_hub/src/Components/GameCard.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Game } from '../Hooks/useGames'
-import { Card, Image, CardBody, Heading, Text, HStack } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImage_URL from '../Services/Image_URL'
+import React from "react";
+import { Game } from "../Hooks/useGames";
+import { Card, Image, CardBody, Heading, Text, HStack } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImage_URL from "../Services/Image_URL";
 
-interface props
-{
-  game: Game
+interface props {
+  game: Game;
 }
 
 const GameCard = ({ game }: props) => {
   return (
-	 <>
-	 <Card >
-		<Image src={getCroppedImage_URL(game.background_image)}></Image>
-		<CardBody>
-			<Heading fontSize='xl'>{game.name}</Heading>
-			<HStack justifyContent='space-between'>
-				<PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconList>{/* // constructing an  array of platform object */}
-				<CriticScore score={game.metacritic}></CriticScore>
-			</HStack>
-		</CardBody>
-	 </Card>
-	 </>
-  )
-}
+    <>
+      <Card>
+        <Image src={getCroppedImage_URL(game.background_image)}></Image>
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom='3px'>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            ></PlatformIconList>
+            {/* // constructing an  array of platform object */}
+            <CriticScore score={game.metacritic}></CriticScore>
+          </HStack>
+          <Heading fontSize="xl">{game.name}</Heading>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
 
-export default GameCard
+export default GameCard;
